41806586555c9f6e7bc6fc12e7c106911f3a877c
yamamotyyie <remuxia1415erina@gmail.com>
2021-02-24 21:14:40 +0900

    マイグレーションファイルを作成

--- db/migrate/20210224120320_create_illusts.rb
+++ db/migrate/20210224120320_create_illusts.rb
@@ -1,7 +1,9 @@
 class CreateIllusts < ActiveRecord::Migration[6.0]
   def change
     create_table :illusts do |t|
-
+      t.string :title,null: false
+      t.text :text,null:false
+      t.references :user, foreign_key: true, null: false
       t.timestamps
     end
   end
