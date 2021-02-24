0cc116b5d00d713da859987aa9b04ca8ed601f5e
yamamotyyie <remuxia1415erina@gmail.com>
2021-02-24 21:27:00 +0900

    投稿機能のルーティング、コントローラー

--- app/controllers/illusts_controller.rb
+++ app/controllers/illusts_controller.rb
@@ -1,4 +1,11 @@
 class IllustsController < ApplicationController
   def index
   end
+
+  def new
+    
+  end
+  def create
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "illusts#index"
+  resources :illusts,only:[:new,:create]
 end
