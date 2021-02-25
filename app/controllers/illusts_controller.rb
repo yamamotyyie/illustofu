0305901ce974f05ac8142ca21312469e4fb07552
yamamotyyie <remuxia1415erina@gmail.com>
2021-02-25 23:05:23 +0900

    イラスト投稿機能の実装

--- app/controllers/illusts_controller.rb
+++ app/controllers/illusts_controller.rb
@@ -3,9 +3,19 @@ class IllustsController < ApplicationController
   end
 
   def new
-    
+    @illust = Illust.new
   end
   def create
-    
+    @illust = Illust.new(illust_params)
+    if @illust.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def illust_params
+    params.require(:illust).permit(:title,:text,:image).merge(user_id: current_user.id)
   end
 end
