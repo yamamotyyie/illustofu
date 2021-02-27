a89b4a59726353d54f910f7f39a552133d06693b
yamamotyyie <remuxia1415erina@gmail.com>
2021-02-27 17:26:30 +0900

    コメントモデル、テーブルの作成

---
+++ db/migrate/20210227082007_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.text :text,null:false
+      t.references :user, foreign_key: true, null: false
+      t.references :illust, foreign_key: true, null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_102728) do
+ActiveRecord::Schema.define(version: 2021_02_27_082007) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2021_02_25_102728) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id", null: false
+    t.bigint "illust_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["illust_id"], name: "index_comments_on_illust_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "illusts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -56,5 +66,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_102728) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "illusts"
+  add_foreign_key "comments", "users"
   add_foreign_key "illusts", "users"
 end
