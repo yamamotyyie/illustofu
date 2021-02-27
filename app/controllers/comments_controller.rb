60847915932aa744ae9b5d12f1e9bf0bd1aef54a
yamamotyyie <remuxia1415erina@gmail.com>
2021-02-27 19:02:41 +0900

    commentコントローラー記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+  end
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id:current_user.id,illust_id: params[:illust_id])
+  end
 end
