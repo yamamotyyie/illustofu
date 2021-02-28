24c6eb46db4d852a8d55b4c95edf38a6ff477b1b
yamamotyyie <remuxia1415erina@gmail.com>
2021-02-28 18:53:46 +0900

    テスト用のダミー画像

--- spec/factories/illusts.rb
+++ spec/factories/illusts.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :illust do
     title     {"テスト"}
     text     {"タイトル"}
+
+    after(:build) do |illust|
+      illust.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+    association :user
   end
 end
