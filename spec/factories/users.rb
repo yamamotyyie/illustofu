a157ff264946c5b54c7d107f60acd54f8eddfb46
yamamotyyie <remuxia1415erina@gmail.com>
2021-02-28 18:02:47 +0900

    ユーザーテストfactory記入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname                  {"とーふ"}
+    email                     {Faker::Internet.free_email}
+    password                  {"test123"}
+    password_confirmation     {password}
   end
 end
