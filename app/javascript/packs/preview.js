3d75f9ae9d1318381da54436a3bc5cbbc1348316
yamamotyyie <remuxia1415erina@gmail.com>
2021-03-06 23:21:01 +0900

    Update preview.js

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -7,5 +7,7 @@ document.addEventListener('DOMContentLoaded',function(){
    const imageElement = document.createElement('div');
    const blobImage = document.createElement('img');
    blobImage.setAttribute('src',blob);
+
+   imageElement.appendChild(blobImage)
   });
 });
