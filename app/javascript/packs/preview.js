0f76ae00d3098314377d5c5e1cdd5456885e0377
yamamotyyie <remuxia1415erina@gmail.com>
2021-03-07 20:56:42 +0900

    Update preview.js

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,6 +1,11 @@
 if (document.URL.match( /new/) || document.URL.match( /edit/)) {
 document.addEventListener('DOMContentLoaded',function(){
   const ImageList = document.getElementById('image-list');
+
+  const createImageHTML = (blob) => {
+    const imageElement = document.createElement('div');
+  }
+
   document.getElementById('illust_image').addEventListener('change',function(e){
    const imageContent = document.querySelector('img');
    if (imageContent){
@@ -9,7 +14,6 @@ document.addEventListener('DOMContentLoaded',function(){
    const file=e.target.files[0];
    const blob= window.URL.createObjectURL(file);
 
-   const imageElement = document.createElement('div');
    const blobImage = document.createElement('img');
    blobImage.setAttribute('src',blob);
 
