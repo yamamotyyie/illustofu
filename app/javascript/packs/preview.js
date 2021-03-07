b9e874b8b28234c29797a49c557a941a06caed74
yamamotyyie <remuxia1415erina@gmail.com>
2021-03-07 20:28:12 +0900

    プレビュー

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,6 +2,10 @@ if (document.URL.match( /new/) || document.URL.match( /edit/)) {
 document.addEventListener('DOMContentLoaded',function(){
   const ImageList = document.getElementById('image-list');
   document.getElementById('illust_image').addEventListener('change',function(e){
+   const imageContent = document.querySelector('img');
+   if (imageContent){
+     imageContent.remove();
+   }
    const file=e.target.files[0];
    const blob= window.URL.createObjectURL(file);
 
