ee63cff1ea36762cb412cb4958c408c2642aaf2c
yamamotyyie <remuxia1415erina@gmail.com>
2021-04-02 20:46:49 +0900

    Update menu.js

--- app/javascript/packs/menu.js
+++ app/javascript/packs/menu.js
@@ -2,6 +2,6 @@ window.addEventListener('load',function(){
   const pullDownButton = document.getElementById("lists")
 
   pullDownButton.addEventListener('mouseover',function(){
-    console.log("mouseover ok");
+    
   })
 })
