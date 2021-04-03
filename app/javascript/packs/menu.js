0628a4d356a06b480c860e0c7fbe6d3cef0cab96
yamamotyyie <remuxia1415erina@gmail.com>
2021-04-03 22:50:32 +0900

    Update menu.js

--- app/javascript/packs/menu.js
+++ app/javascript/packs/menu.js
@@ -2,6 +2,10 @@ window.addEventListener('load',function(){
   const pullDownButton = document.getElementById("lists")
 
   pullDownButton.addEventListener('mouseover',function(){
-    
+    console.log("mouseover OK");
+  })
+
+  pullDownButton.addEventListener('mouseout',function(){
+    console.log("mouseout OK");
   })
 })
