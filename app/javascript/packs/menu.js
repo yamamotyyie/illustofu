6c1eff9eeb7fd03e596f12f0b6f7b5ef7c75d574
yamamotyyie <remuxia1415erina@gmail.com>
2021-04-07 17:45:29 +0900

    Update menu.js

--- app/javascript/packs/menu.js
+++ app/javascript/packs/menu.js
@@ -7,4 +7,7 @@ window.addEventListener('load',function(){
   pullDownButton.addEventListener('mouseout',function(){
     console.log("mouseout OK");
   })
+  pullDownButton.addEventListener('click',function(){
+    console.log("click OK");
+  })
 })
