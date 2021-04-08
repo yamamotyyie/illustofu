5b4b2b95e41fe14d6fe2e6367402df8c72164f3d
yamamotyyie <remuxia1415erina@gmail.com>
2021-04-08 20:50:41 +0900

    Update menu.js

--- app/javascript/packs/menu.js
+++ app/javascript/packs/menu.js
@@ -1,5 +1,6 @@
 window.addEventListener('load',function(){
   const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
   
   pullDownButton.addEventListener('mouseover',function(){
     pullDownButton.setAttribute("style","background-color:gray;")
