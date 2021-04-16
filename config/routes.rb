2f8b63f211f62159b9962886fc5b795fd19e4b9e
yamamotyyie <remuxia1415erina@gmail.com>
2021-04-16 19:01:20 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "illusts#index"
+  resources :users , only:[:show]
   resources :illusts do
   resources :comments, only: [:create]
   end
