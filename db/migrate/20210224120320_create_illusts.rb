d949cdc0939d068970ffb2ba05ea6795dc3bc1f1
yamamotyyie <remuxia1415erina@gmail.com>
2022-06-06 21:00:45 +0900

    Update 20210224120320_create_illusts.rb

--- db/migrate/20210224120320_create_illusts.rb
+++ db/migrate/20210224120320_create_illusts.rb
@@ -1,11 +1,11 @@
 class CreateIllusts < ActiveRecord::Migration[6.0]
+  # 各カラムのレコードを指定
   def change
     create_table :illusts do |t|
       t.string :title,null: false
       t.text :text,null:false
       t.references :user, foreign_key: true, null: false
       t.timestamps
-      # 各カラムのレコードを指定
     end
   end
 end
