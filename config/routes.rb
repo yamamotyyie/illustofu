d1771642d615acbe88904444ea28091c72d177fb
yamamotyyie <remuxia1415erina@gmail.com>
2022-06-06 20:57:02 +0900

    編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  # deviseの時に使う
   devise_for :users
+  # トップページへのルート
   root to: "illusts#index"
+  # userの詳細機能のみをresourcesから取り出す
   resources :users , only:[:show]
+  # イラストにコメントをネスト。どのイラストにコメントをつけるか
   resources :illusts do
   resources :comments, only: [:create]
   end
