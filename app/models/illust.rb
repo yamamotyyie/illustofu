450f943e4f091327fbe071fdb009b6d882c21674
yamamotyyie <remuxia1415erina@gmail.com>
2022-06-06 21:12:20 +0900

    Update illust.rb

--- app/models/illust.rb
+++ app/models/illust.rb
@@ -1,8 +1,11 @@
 class Illust < ApplicationRecord
+  # 一つのイラストは一人のユーザーを持つ
   belongs_to :user
+  # 一つのイラストは複数のコメントを持つ。イラストが消えるとコメントも消える
   has_many :comments , dependent: :destroy
+  # イラストと画像の結びつけ
   has_one_attached :image
-
+  # 各バリデーション
   with_options presence: true do
     validates :title
     validates :text
