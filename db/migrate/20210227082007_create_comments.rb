695ba3fb9c8d58cda157e7b034fddfc6818f4df2
yamamotyyie <remuxia1415erina@gmail.com>
2022-06-06 21:05:22 +0900

    Update 20210227082007_create_comments.rb

--- db/migrate/20210227082007_create_comments.rb
+++ db/migrate/20210227082007_create_comments.rb
@@ -2,8 +2,11 @@ class CreateComments < ActiveRecord::Migration[6.0]
   # コメントのテーブル設定
   def change
     create_table :comments do |t|
+      # コメントのレコード設定
       t.text :messeage,null:false
+      # userテーブルと結びつけ
       t.references :user, foreign_key: true, null: false
+      # illustテーブルと結びつけ
       t.references :illust, foreign_key: true, null: false
       t.timestamps
     end
