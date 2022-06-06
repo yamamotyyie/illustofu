95c4cc3d21a2293e23ef1f4cfdab69816c1594fd
yamamotyyie <remuxia1415erina@gmail.com>
2022-06-06 21:02:21 +0900

    Update 20210224120320_create_illusts.rb

--- db/migrate/20210224120320_create_illusts.rb
+++ db/migrate/20210224120320_create_illusts.rb
@@ -2,8 +2,11 @@ class CreateIllusts < ActiveRecord::Migration[6.0]
   # 各カラムのレコードを指定
   def change
     create_table :illusts do |t|
+      # イラストのタイトルは必須
       t.string :title,null: false
+      # テキストも必須
       t.text :text,null:false
+      # userテーブルと結びつけ
       t.references :user, foreign_key: true, null: false
       t.timestamps
     end
